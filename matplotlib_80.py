0bb53217f808e20e853ff91c625717f80d90694a
Haryourlah <105593593+Haryourlah@users.noreply.github.com>
2022-05-18 09:02:57 +0100

    Update matplotlib_80.py

--- matplotlib_80.py
+++ matplotlib_80.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-xpoints = np.array([5, 30, 5])
+xpoints = np.array([5, 10, 15, 20, 25, 30])
 ypoints = np.array([96, 83, 78, 60, 52, 30])
 
 plt.plot(xpoints, ypoints)
